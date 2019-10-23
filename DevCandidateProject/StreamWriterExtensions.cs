11194141ebea1ce7932e22f47aa8d0f2530064e2
jacksonbowen <42704535+jacksonbowen@users.noreply.github.com>
2019-10-23 01:53:02 -0400

    changed formatting of comments

--- DevCandidateProject/StreamWriterExtensions.cs
+++ DevCandidateProject/StreamWriterExtensions.cs
@@ -4,22 +4,22 @@ using System.IO;
 namespace DevCandidateProject
 {
 	/// <summary>
-	///		Extension methods for the <see cref="StreamWriter"/> type.
+	/// Extension methods for the <see cref="StreamWriter"/> type.
 	/// </summary>
 	public static class StreamWriterExtensions
 	{
 		/// <summary>
-		///		Extension method for <see cref="StreamWriter"/> objects to write the <see cref="text"/> to
-		///		both the provided <see cref="StreamWriter"/>, as well as to the <see cref="Console"/>.
+		/// Extension method for <see cref="StreamWriter"/> objects to write the <see cref="text"/> to
+		/// both the provided <see cref="StreamWriter"/>, as well as to the <see cref="Console"/>.
 		/// </summary>
 		/// <param name="this">
-		///		The <see cref="StreamWriter"/> in which to write to.
+		/// The <see cref="StreamWriter"/> in which to write to.
 		/// </param>
 		/// <param name="text">
-		///		The text to write to both the <see cref="StreamWriter"/> and the <see cref="Console"/>.
+		/// The text to write to both the <see cref="StreamWriter"/> and the <see cref="Console"/>.
 		/// </param>
 		/// <returns>
-		///		Returns the original <see cref="StreamWriter"/> object for method chaining.
+		/// Returns the original <see cref="StreamWriter"/> object for method chaining.
 		/// </returns>
 		public static StreamWriter WriteLineEx(
 			this StreamWriter @this,
