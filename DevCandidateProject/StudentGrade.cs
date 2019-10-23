f7c737c5cb0684ddeb794909bd38dd32df38f0b5
jacksonbowen <42704535+jacksonbowen@users.noreply.github.com>
2019-10-23 01:54:16 -0400

    changed formatting of comments

--- DevCandidateProject/StudentGrade.cs
+++ DevCandidateProject/StudentGrade.cs
@@ -3,24 +3,24 @@
 namespace DevCandidateProject
 {
 	/// <summary>
-	///		The <see cref="StudentGrade"/> class represents one student, their <see cref="Grade"/> (a
-	///		<see cref="double"/> between 0 and 100, inclusively), and their <see cref="AdjustedGrade"/>,
-	///		an <see cref="int"/> adjustment dropping off any decimals.
+	/// The <see cref="StudentGrade"/> class represents one student, their <see cref="Grade"/> (a
+	/// <see cref="double"/> between 0 and 100, inclusively), and their <see cref="AdjustedGrade"/>,
+	/// an <see cref="int"/> adjustment dropping off any decimals.
 	/// </summary>
 	public class StudentGrade
 	{
 		/// <summary>
-		///		The student's full name.
+		/// The student's full name.
 		/// </summary>
 		public string StudentName { get; }
 
 		/// <summary>
-		///		The student's unadjusted <see cref="double"/> grade, including any decimals.
+		/// The student's unadjusted <see cref="double"/> grade, including any decimals.
 		/// </summary>
 		public double Grade { get; }
 
 		/// <summary>
-		///		The student's adjusted <see cref="int"/> grade, dropping any decimals.
+		/// The student's adjusted <see cref="int"/> grade, dropping any decimals.
 		/// </summary>
 		public int AdjustedGrade
 		{
@@ -29,13 +29,13 @@ namespace DevCandidateProject
 
 
 		/// <summary>
-		///		The constructor for the <see cref="StudentGrade"/> class.
+		/// The constructor for the <see cref="StudentGrade"/> class.
 		/// </summary>
 		/// <param name="studentName">
-		///		The student's full name.
+		/// The student's full name.
 		/// </param>
 		/// <param name="grade">
-		///		The student's unadjusted <see cref="double"/> grade, including any decimals.
+		/// The student's unadjusted <see cref="double"/> grade, including any decimals.
 		/// </param>
 		public StudentGrade(
 			string studentName,
